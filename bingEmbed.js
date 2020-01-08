850d3c6ab91c73401652ea81635ee03d08bcc1a1
Marc8019 <57566905+Marc8019@users.noreply.github.com>
2020-01-07 21:39:04 -0500

    Added bookmarklets

---
+++ bingEmbed.js
@@ -0,0 +1,5 @@
+//turns your current website into a bing iframe. All browsing is contained in the iframe. many sites don't allow iframes,
+//and will refuse connection. Html games work, no flash. 
+//Krunker.io works.
+//Change the url to a proxy if you want more functionality. 
+javascript:document.body.innerHTML = "<iframe src=\"https:\/\/bing.com\/\" style=\"border:0px #ffffff none;\" name=\"myiFrame\" scrolling=\"yes\" frameborder=\"1\" marginheight=\"0px\" marginwidth=\"0px\" height=\"100%\" width=\"100%\" allowfullscreen><\/iframe>";
