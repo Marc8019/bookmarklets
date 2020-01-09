4ad8890e63ad983d63819249d2e7957abc577165
Marc <57566905+Marc8019@users.noreply.github.com>
2020-01-09 11:05:37 -0500

    Create inspect-element.js

---
+++ inspect-element.js
@@ -0,0 +1,2 @@
+//This lets you edit text in a website. Execute the bookmarklet and all texts will act as fields that you can edit. Execute the 
+//bookmarklet again to turn it off and lock in the edits.
