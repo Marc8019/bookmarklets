df07f82345e133ecb9ca74b3ad3ac1c92a6d6060
Jax <61421101+JaxGamez@users.noreply.github.com>
2021-12-03 09:11:53 -0600

    Update EmProxy.js

--- EmProxy.js
+++ EmProxy.js
@@ -1,3 +1,3 @@
 // Essentially deletes all code from the website you are visiting and replacing it with an iframe embed of the proxy which can lead you to the website of your choice
 // Copy the code below and paste it into a new bookmark as the 'URL', name it whatever you want.
-javascript:document.body.innerHTML = "<iframe src=\"https://z99.herokuapp.com/" style=\"border:0px #ffffff none;\" name=\"myiFrame\" scrolling=\"yes\" frameborder=\"1\" marginheight=\"0px\" marginwidth=\"0px\" height=\"100%\" width=\"100%\" allowfullscreen><\/iframe>";
+javascript:document.body.innerHTML = "<iframe src=\"https:\/\/z99.herokuapp.com\/\" style=\"border:0px #ffffff none;\" name=\"myiFrame\" scrolling=\"yes\" frameborder=\"1\" marginheight=\"0px\" marginwidth=\"0px\" height=\"100%\" width=\"100%\" allowfullscreen><\/iframe>";
