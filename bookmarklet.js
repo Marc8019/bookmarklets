e8df141f152191e2739c9e71d092c1e1590c37ae
Jax <61421101+JaxGamez@users.noreply.github.com>
2022-04-14 09:08:17 -0500

    beginning of new

---
+++ bookmarklet.js
@@ -0,0 +1,3 @@
+// create a bookmark (ctrl+d) and replace the link with the code below :D
+// made by jax
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='LINK HERE' })();
