3eb22fefe27877acb2a06a756782ffea05fb9ac9
Jax <61421101+JaxGamez@users.noreply.github.com>
2022-04-15 10:32:50 -0500

    wrong link in code, fix

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,3 @@
 // create a bookmark (ctrl+d) and replace the link with the code below :D
 // made by jax
-javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://unbl0ck3d.github.io/bookmarklets/menu.html' })();
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://github.com/unbl0ck3d/bookmarklets/raw/bookmarklet-storage/new-site.js' })();
