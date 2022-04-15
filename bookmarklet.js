4ff377713762bec2ced2edc7f595544215904057
Jax <61421101+JaxGamez@users.noreply.github.com>
2022-04-15 10:28:04 -0500

    bookmarklet beta

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,3 @@
 // create a bookmark (ctrl+d) and replace the link with the code below :D
 // made by jax
-javascript:(function(){document.body.appendChild(document.createElement('script')).src='LINK HERE' })();
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://unbl0ck3d.github.io/bookmarklets/menu.html' })();
